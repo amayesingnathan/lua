5cb60d180571b0983a562b6e2f2fd23cd9008f74
amayesingnathan <92682126+amayesingnathan@users.noreply.github.com>
2022-06-12 21:40:51 +0100

    Removed onelua.c

--- premake5.lua
+++ premake5.lua
@@ -3,12 +3,12 @@ project "lua"
     language "C++"
     cppdialect "C++17"
     staticruntime "on"
-		
-    targetdir 	("%{wks.location}/bin/%{prj.name}/" .. outputDir)
-    objdir 		("%{wks.location}/obj/%{prj.name}/" .. outputDir)
+
+    targetdir   ("%{wks.location}/bin/%{prj.name}/" .. outputDir)
+    objdir      ("%{wks.location}/obj/%{prj.name}/" .. outputDir)
 
     files "lua.hpp"
-	
+
     removefiles { "onelua.c" }
 
     filter "system:windows"
@@ -18,39 +18,41 @@ project "lua"
             "src/*.h", 
             "src/*.c"
         }
+        removefiles "src/onelua.c"
         
-	filter "system:linux"
+    filter "system:linux"
         pic "On"
         systemversion "latest"
         files 
-        { 
+        {
             "*.h", 
             "*.c"
         }
+        removefiles "onelua.c"
 
-	filter "system:linux or bsd or hurd or aix or solaris or haiku"
-		defines     { "LUA_USE_POSIX", "LUA_USE_DLOPEN" }
+    filter "system:linux or bsd or hurd or aix or solaris or haiku"
+        defines     { "LUA_USE_POSIX", "LUA_USE_DLOPEN" }
         
     filter "configurations:x64d"
-		runtime "Debug"
+        runtime "Debug"
         symbols "on"
 
-	filter "configurations:ARMd"
-		runtime "Debug"
+    filter "configurations:ARMd"
+        runtime "Debug"
         symbols "on"
 
-	filter "configurations:ARM64d"
-		runtime "Debug"
-		symbols "on"
+    filter "configurations:ARM64d"
+        runtime "Debug"
+        symbols "on"
 
     filter "configurations:x64"
-		runtime "Release"
+        runtime "Release"
         optimize "on"
 
-	filter "configurations:ARM"
-		runtime "Release"
-		optimize "on"
+    filter "configurations:ARM"
+        runtime "Release"
+        optimize "on"
 
-	filter "configurations:ARM64"
-		runtime "Release"
-		optimize "on"
+    filter "configurations:ARM64"
+        runtime "Release"
+        optimize "on"
